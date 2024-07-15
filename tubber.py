d40c5a0c8cdfd4d54a0c535563872f7a6dc8c52f
Matheus Silva <124784864+omaatheus@users.noreply.github.com>
2024-07-15 17:00:33 -0300

    🔨 refactor

--- tubber.py
+++ tubber.py
@@ -1,15 +1,12 @@
-
 kmRodado = float(input('Insira a Quilometragem rodada: '))
 
-rodado5Km = f'R${kmRodado * 5.00:.2f}'
-rodado5and15Km = f'R${kmRodado * 4.50:.2f}'
-rodado15KmMais = f'R${kmRodado * 4.00:.2f}'
-
 if(kmRodado < 5):
-    print(f'Foram rodados {kmRodado}km e o valor total da viagem foi de {rodado5Km}')
+    valor = f'R${kmRodado * 5.00:.2f}'
     
-if(kmRodado >= 5) and (kmRodado < 15):
-    print(f'Foram rodados {kmRodado}km e o valor total da viagem foi de {rodado5and15Km}')
+elif(kmRodado >= 5) and (kmRodado <= 15):
+   valor = f'R${kmRodado * 4.50:.2f}'
     
-if(kmRodado >= 15):
-    print(f'Foram rodados {kmRodado}km e o valor total da viagem foi de {rodado15KmMais}')
+else:
+    valor = f'R${kmRodado * 4.00:.2f}'
+    
+print(f'Foram rodados {kmRodado}km e o valor foi de {valor}')
